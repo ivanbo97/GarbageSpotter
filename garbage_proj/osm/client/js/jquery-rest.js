067a944da704ebb80f00e4e9163208e8a468e0f7
ivanbo97 <61833244+ivanbo97@users.noreply.github.com>
2020-09-03 12:14:12 +0300

    Update jquery-rest.js

--- garbage_proj/osm/client/js/jquery-rest.js
+++ garbage_proj/osm/client/js/jquery-rest.js
@@ -1,11 +1,3 @@
-// Shorthand for SELETE AJAX requests.
-$.delete = function (url) {
-	return $.ajax({
-		url: url,
-		type: 'DELETE'
-	});
-};
-
 // Shorthand for POST AJAX requests with JSON.
 // Same as $.post but with object-to-JSON conversion.
 $.postJSON = function (url, data) {
@@ -17,21 +9,3 @@ $.postJSON = function (url, data) {
 	});
 };
 
-// Usage: $('form.person-edit').serializeObject();
-// Goes through all fields in the form, and collects their values.
-// returns object with variables and values that correspond to the form fields.
-$.fn.serializeObject = function () {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function () {
-		if (o[this.name]) {
-			if (!o[this.name].push) {
-				o[this.name] = [o[this.name]];
-			}
-			o[this.name].push(this.value || '');
-		} else {
-			o[this.name] = this.value || '';
-		}
-	});
-	return o;
-};
